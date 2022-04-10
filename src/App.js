29d9b0afb998ff151598143089c7b4bff04a413c
João PV Correia <correia.jpv@gmail.com>
2022-04-10 17:48:05 +0100

    🎉 Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Suspense } from "react";
+import Footer from "./UI/footer";
+import "./styles/App.css";
+
+const ResetCountdownTimers = React.lazy(() =>
+	import("./UI/resetCountdownTimer")
+);
+
+const App = () => {
+	return [
+		<h1 className="text-center">
+			Lost Ark EU/NA weekly and daily reset times and countdown
+		</h1>,
+		<Suspense fallback={<div>Loading timer...</div>}>
+			<ResetCountdownTimers />,
+		</Suspense>,
+		<Footer />,
+	];
+};
+
+export default App;
